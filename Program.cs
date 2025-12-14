1d882a10650ad7881a18aeca7af4d77055d31816
Pupler <misha.reak45@gmail.com>
2025-12-14 19:21:21 +0100

    Feat: implement menu navigation

--- Program.cs
+++ Program.cs
@@ -3,6 +3,22 @@ using System.Runtime.InteropServices;
 
 class Program
 {
+    static void AddFinancialGoal()
+    {
+        Console.WriteLine("AddFinancialGoal");
+    }
+    static void ShowGoals()
+    {
+        Console.WriteLine("showGoals");
+    }
+    static void AddExpense()
+    {
+        Console.WriteLine("AddExpense");
+    }
+    static void ShowAnalytics()
+    {
+        Console.WriteLine("ShowAnalytics");
+    }
     static void Main()
     {
         while (true)
@@ -16,7 +32,34 @@ class Program
             Console.WriteLine("0. Exit");
             Console.Write("\nChoose option: ");
             string? input = Console.ReadLine();
-            Console.WriteLine($"You chose: {input}");
+            
+            if (int.TryParse(input, out int choice))
+            {
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Closing program...");
+                        return;
+                    case 1:
+                        AddFinancialGoal();
+                        break;
+                    case 2:
+                        ShowGoals();
+                        break;
+                    case 3:
+                        AddExpense();
+                        break;
+                    case 4:
+                        ShowAnalytics();
+                        break;
+                    default:
+                        Console.WriteLine("Error: Wrong menu index!");
+                        break;
+                }
+            } else
+            {
+                Console.WriteLine("Error: Write a number!");
+            }
         }
     }
 }
